f5f49142af993e802ca8347af5b946b0c517498e
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2024-07-16 18:13:30 +0200

    Added getAirport and postAirport & refactor API to use generic function

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,16 +2,33 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:1234';
 
-export interface Airport {
+type Airport = {
   id: number;
   iata: string;
   name: string;
+};
+
+type PostAirport = Omit<Airport, 'id'>;
+
+export async function getAirports(): Promise<Airport[]> {
+  return await fetchData('/airports', 'get');
 }
 
-export async function apiGetAirports(): Promise<Airport[]> {
+export async function getAirport(id: number): Promise<Airport> {
+  return await fetchData(`/airports/${id}`, 'get');
+}
+
+export async function postAirport(): Promise<PostAirport> {
+  return await fetchData<Airport>('/airports', 'post');
+}
+
+async function fetchData<T>(
+  endpoint: `/${string}`,
+  method: 'get' | 'post' | 'put' | 'patch' | 'delete'
+): Promise<T> {
   try {
-    const response = await axios.get<Airport[]>(`${API_URL}/airports`);
-    return response.data;
+    const response = await axios[method]<T>(`${API_URL}${endpoint}`);
+    return response.data as T;
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
