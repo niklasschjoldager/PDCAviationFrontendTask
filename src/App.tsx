eefe615297ea1d156fcae49d0ae46b8eef5ea2a7
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2024-07-17 10:38:14 +0200

    Moved React Query to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './App.scss';
 import Airports from './components/Airports';
 
-const queryClient = new QueryClient();
-
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <main>
-        <Airports />
-      </main>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <main>
+      <Airports />
+    </main>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './scss/index.scss';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
